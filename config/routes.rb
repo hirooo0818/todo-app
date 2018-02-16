5546e1aac9672b486f634760553f6f7ba975fc27
hiroyuki.hasegawa <hiroyuki.hasegawa@mixi.co.jp>
2018-02-16 20:11:26 +0900

    vue-routerでSPA構築

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  get '/about', to: 'home#index'
+  get '/contact', to: 'home#index'
+
   namespace :v1, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
   end
